06d2647bf9be260a1dd6b20825139c7de46fe539
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-12 10:22:39 -0300

    build: Adicionado exemplos de array

---
+++ 00-exercicios/ruby-na-pratica-exercicios/exemplos/10-1.rb
@@ -0,0 +1,17 @@
+meu_array = [10, 20, 30, 40, 50]
+
+meu_array.each do |elemento|
+  puts elemento
+end
+
+meu_array = [10, 20, 30, 40, 50]
+
+meu_array.each { |elemento|
+  puts elemento
+}
+
+meu_array = [10, 20, 30, 40, 50]
+
+for elemento in meu_array
+  puts elemento
+end
