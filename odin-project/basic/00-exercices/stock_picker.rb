11050ab6558af935fa6455900dae69c2f03d12b8
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-13 09:42:05 -0300

    build: add OdinProject Exercices ruby

---
+++ odin-project/basic/00-exercices/stock_picker.rb
@@ -0,0 +1,35 @@
+# Implement a method #stock_picker that takes in an array
+# of stock prices, one for each hypothetical day.
+# It should return a pair of days representing the
+# best day to buy and the best day to sell. Days start at 0.end
+
+# Quick Tips:
+
+# You need to buy before you can sell
+# Pay attention to edge cases like when the
+# lowest day is the last day or the highest day is the first day.
+
+def stock_picker(prices)
+  return [] if prices.empty?
+
+  min_price = prices[0]
+  min_day = 0
+  max_profit = 0
+  best_days = [0, 0]
+
+  prices.each_with_index do |price, day|
+    if price < min_price
+      min_price = price
+      min_day = day
+    elsif price - min_price > max_profit
+      max_profit = price - min_price
+      best_days = [min_day, day]
+    end
+  end
+
+  best_days
+end
+
+# Exemplo de uso
+stock_prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
+p stock_picker(stock_prices) # => [1, 4]
---
+++ odin-project/basic/00-exercices/sub_strings.rb
@@ -0,0 +1,28 @@
+# Implement a method #substrings that takes a word as
+# the first argument and then an array of valid substrings
+# (your dictionary) as the second argument. It should
+# return a hash listing each substring (case insensitive)
+# that was found in the original string and how many times it was found.
+
+# Next, make sure your method can handle multiple words:
+# Please note the order of your keys might be different.
+# Quick Tips:
+# Recall how to turn strings into arrays and arrays into strings.
+
+def substrings(senteces, dictionary)
+  word_count = Hash.new(0)
+
+  words = senteces.downcase.split
+
+  words.each do |word|
+    dictionary.each do |substring|
+      if word.include?(substring)
+        word_count[substring] += 1
+      end
+    end
+  end
+  word_count
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
