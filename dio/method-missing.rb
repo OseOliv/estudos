323960ed7abd0c396397eb4121d54208873c991b
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-14 09:41:16 -0300

    build: add course method-missing

---
+++ dio/method-missing.rb
@@ -0,0 +1,16 @@
+class Fish
+
+def method_missing(method_name)
+  puts "Peixe nao pode #{method_name} ainda"
+end
+
+def nadar
+  puts "O peixe esta nadando"
+end
+
+end
+
+fish = Fish.new
+fish.nadar
+fish.andar
+fish.chutar
