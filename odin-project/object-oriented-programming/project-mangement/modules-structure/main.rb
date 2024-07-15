54cb9b992234611ae4a1f94702bacd8158bd1827
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-15 16:30:54 -0300

    build: add poo OdinProject Course

---
+++ odin-project/object-oriented-programming/project-mangement/modules-structure/main.rb
@@ -0,0 +1,7 @@
+require_relative 'lib/not_so_green'
+require_relative 'lib/scheals'
+
+puts NotSoGreen.food_opinion('Cereal')
+#=> Cereal is awesome!
+puts Scheals.food_opinion('Marmite')
+#=> Marmite is awful!
